9ccfabe56be789c319e44549bebee375cf0a1047
Mr. Grigory <gymoroz@icloud.com>
2026-03-18 16:55:16 +0300

    finish domain model for default schedule

--- src/EmployeesScheduler.Domain/Schedule/WorkSchedule.cs
+++ src/EmployeesScheduler.Domain/Schedule/WorkSchedule.cs
@@ -4,37 +4,31 @@ using MrGrigory.MyUtils;
 
 namespace EmployeesScheduler.Domain.Schedule;
 
+/// <summary>
+/// Дефолтный график сотрудника
+/// </summary>
 public class WorkSchedule
 {
-    private WorkSchedule(ICollection<CycleInterval> intervals, Guid employeeId, DateOnly startDate)
-    {
-        Id = Guid.NewGuid();
-        _intervals = intervals.ToList();
-        EmployeeId = employeeId;
-        StartDate = startDate;
-    }
-    
     private readonly List<CycleInterval> _intervals;
 
     public Guid Id { get; }
     public Guid EmployeeId { get; private set; }
     
-    public DateOnly StartDate { get; private set; }
+    public DateTime StartDate { get; private set; }
 
     public IReadOnlyCollection<CycleInterval> Intervals => _intervals;
 
-
-    public static Result<WorkSchedule, Error> Create(ICollection<CycleInterval> intervals, Guid employeeId, DateOnly startDate)
+    
+    private WorkSchedule(ICollection<CycleInterval> intervals, Guid employeeId, DateOnly startDate)
     {
-        // intervals validation
-        var intervalValidateResult = ValidateIntervals(intervals);
-        if (intervalValidateResult.IsFailure)
-            return intervalValidateResult.Error;
-        var validIntervals = intervalValidateResult.Value;
-        
-        return new WorkSchedule(validIntervals, employeeId, startDate);
+        Id = Guid.NewGuid();
+        _intervals = intervals.ToList();
+        EmployeeId = employeeId;
+        StartDate = DateTime.SpecifyKind(
+            startDate.ToDateTime(TimeOnly.MinValue),
+            DateTimeKind.Utc);
     }
-
+    
     private static Result<List<CycleInterval>, Error> ValidateIntervals(ICollection<CycleInterval> intervals)
     {
         if (intervals.Count == 0)
@@ -44,7 +38,7 @@ public class WorkSchedule
         
         var items = intervals.OrderBy(x => x.OffsetFromCycleStart).ToList();
         
-        TimeSpan expectedStart = TimeSpan.Zero;
+        var expectedStart = TimeSpan.Zero;
         
         foreach (var interval in items)
         {
@@ -61,6 +55,69 @@ public class WorkSchedule
         return items;
     }
     
-    
+    /// <summary>
+    /// Метод создания нового шаблона графика. Приоритет отдается графику с самой свежей startDate
+    /// </summary>
+    /// <param name="intervals">Расписание сотрудника</param>
+    /// <param name="employeeId">Id сотрудника</param>
+    /// <param name="startDate">Дата начала действия графика в UTC</param>
+    /// <returns></returns>
+    public static Result<WorkSchedule, Error> Create(ICollection<CycleInterval> intervals, Guid employeeId, DateOnly startDate)
+    {
+        // intervals validation
+        var intervalValidateResult = ValidateIntervals(intervals);
+        
+        if (intervalValidateResult.IsFailure)
+            return intervalValidateResult.Error;
+        var validIntervals = intervalValidateResult.Value;
+        
+        return new WorkSchedule(validIntervals, employeeId, startDate);
+    }
+
+    /// <summary>
+    /// Обновление графика без создания нового расписания.
+    /// </summary>
+    /// <param name="newIntervals">Новое расписание сотрудника</param>
+    /// <returns></returns>
+    public UnitResult<Error> ReplaceSchedule(ICollection<CycleInterval> newIntervals)
+    {
+        //TODO Добавить логику, что бы обновить график можно было только не позже даты его начала (или до его начала)
+        
+        // intervals validation
+        var intervalValidateResult = ValidateIntervals(newIntervals);
+        if (intervalValidateResult.IsFailure)
+            return intervalValidateResult.Error;
+        
+        _intervals.Clear();
+        _intervals.AddRange(intervalValidateResult.Value);
+        
+        return Result.Success<Error>();
+    }
+
+    public Result<WorkState, Error> GetStateAt(DateTime datetime)
+    {
+        var datetimeUtc = datetime.ToUniversalTime();
+
+        if (datetimeUtc < StartDate)
+        {
+            return Error.Validation("WorkSchedule.GetStateAt", 
+                $"The transmitted date {datetimeUtc} cannot be less than the initial date {StartDate}", 
+                field: "datetime");
+        }
+
+        var maxSpan = _intervals.Last().OffsetFromCycleFinish;
+        var difference = datetimeUtc - StartDate;
+        var timeSpan = TimeSpan.FromTicks(difference.Ticks % maxSpan.Ticks);
+
+        var resultInterval = _intervals.FirstOrDefault(x => x.Contains(timeSpan));
+
+        if (resultInterval == null)
+        {
+            return Error.NotFound("WorkSchedule.GetStateAt", "not found TimeSpan in intervals");
+        }
+
+        return resultInterval.State;
+
+    }
     
 }
