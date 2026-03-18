f9e3349cbc21b30b6e7f6f74040d9752dcb046eb
Mr. Grigory <gymoroz@icloud.com>
2026-03-18 17:20:38 +0300

    Complete Issue #1. Basic domain models have been implemented

---
+++ src/EmployeesScheduler.Domain/Employees/Employee.cs
@@ -0,0 +1,34 @@
+﻿using CSharpFunctionalExtensions;
+using MrGrigory.MyUtils;
+
+namespace EmployeesScheduler.Domain.Employees;
+
+public class Employee
+{
+    public Guid Id { get; init; }
+    public string FirstName { get; private set; }
+    public string LastName { get; private set; }
+    public DateTime CreatedAt { get; init; }
+    public DateTime UpdatedAt { get; private set; }
+    
+    private Employee(string firstName, string lastName)
+    {
+        Id = Guid.NewGuid();
+        FirstName = firstName;
+        LastName = lastName;
+        CreatedAt = DateTime.UtcNow;
+        UpdatedAt = DateTime.UtcNow;
+    }
+
+    public static Result<Employee, Error> Create(string firstName, string lastName)
+    {
+        if(string.IsNullOrWhiteSpace(firstName))
+            return Error.Validation("Employee.Create", "First name cannot be empty.", field: "FirstName");
+        if(string.IsNullOrWhiteSpace(lastName))
+            return Error.Validation("Employee.Create", "Last name cannot be empty.", field: "LastName");
+        
+        return new Employee(firstName, lastName);
+    }
+    
+    
+}
---
+++ src/EmployeesScheduler.Domain/Schedule/OverrideInterval.cs
@@ -0,0 +1,38 @@
+﻿using CSharpFunctionalExtensions;
+using MrGrigory.MyUtils;
+
+namespace EmployeesScheduler.Domain.Schedule;
+
+/// <summary>
+/// Переопределяет график по умолчанию
+/// </summary>
+public class OverrideInterval
+{
+    public DateTime  StartAt { get; init; }
+    public DateTime EndAt { get; init; }
+    public WorkState State { get; init; }
+    
+    public bool AppliesTo(DateTime datetime) => datetime >= StartAt && datetime <= EndAt;
+    
+    private OverrideInterval(DateTime startAt, DateTime endAt, WorkState state)
+    {
+        StartAt = startAt;
+        EndAt = endAt;
+        State = state;
+    }
+
+    /// <summary>
+    /// Создает новый экземпляр переопределенного интервала в графике сотруднике.
+    /// </summary>
+    /// <param name="startAt">Дата и время начала действия</param>
+    /// <param name="endAt">Дата и время окончания действия</param>
+    /// <param name="state">Состояние в указанный период</param>
+    /// <returns>Возвращает экземпляр данных или ошибку</returns>
+    public static Result<OverrideInterval, Error> Create(DateTime startAt, DateTime endAt, WorkState state)
+    {
+        if (startAt >= endAt)
+            return Error.Validation("OverrideInterval.Create", "startAt cannot be greater than endAt");
+        
+        return new OverrideInterval(startAt, endAt, state);
+    }
+}
